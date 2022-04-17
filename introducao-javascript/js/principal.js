70d32ffadfdbe02059d940a7c2bdab3ce94157e0
Marcela Marinho <mmrezende1984@gmail.com>
2022-04-17 17:30:49 -0300

    atualização

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -9,7 +9,27 @@ var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 var tdImc = paciente.querySelector (".info-imc");
 
-var imc = peso / (altura * altura);
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-tdImc.textContent = imc;
+if(peso <= 0 || peso >= 1000 ){
+    console.log("Peso Invalido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido";
+}
+if(altura <= 0 || altura>= 3.00){
+    console.log("Altura inválida");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura Inválida";
+}
+
+if( alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+
+}
+
+
+
+
 
